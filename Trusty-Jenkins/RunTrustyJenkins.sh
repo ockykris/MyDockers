ac2abc79c826944d70d8655bbb7f835e98073179
Ocky Kristanto <ocky.kristanto@tomtom.com>
2017-12-25 12:42:40 +0800

    CleanUp

--- Trusty-Jenkins/RunTrustyJenkins.sh
+++ Trusty-Jenkins/RunTrustyJenkins.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker rm -f Master-Jenkins
-docker run -d -p 8080:8080 -p 50000:50000 --name Master-Jenkins -v ${PWD}/VarJenkins:/var/jenkins kristant/trusty-jenkins
+docker run -d -p 8080:8080 -p 50000:50000 --name Master-Jenkins -v ${PWD}/TomTom:/var/jenkins kristant/trusty-jenkins
